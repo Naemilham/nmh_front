e8e57e247e00d03ae0fef067458d18171f12e075
tjrwodnjs <tjrwodnjs99@gmail.com>
2023-11-25 21:26:33 +0900

    fix: api test

--- src/routes/apitest.jsx
+++ src/routes/apitest.jsx
@@ -1,11 +1,28 @@
 import { useEffect } from "react";
 // import {signIn} from "../apis/api";
 import axios from "axios";
+import { getCookie } from "../utils/cookie";
 
 const ApiTest = () => {
   useEffect(() => {
     const getPostAPI = async () => {
-      const response = await axios.get("https://naemilham.site/");
+      const response = await axios.get("http://naemilham.site");
+      // const response = await axios.post("http://naemilham.site/api/accounts/signup/", 
+      //   {
+      //     "username": "user3",
+      //     "password1": "user3user3",
+      //     "password2": "user3user3",
+      //     "email": "idltkim@gmail.com",
+      //     "nickname": "유저3",
+      //     "is_writer": "true"
+      // }, 
+      //   {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     "X-CSRFToken": getCookie("csrftoken"),
+      //   },
+      //   withCredentials: true
+      // });
       console.log(response);
     };
     getPostAPI();
