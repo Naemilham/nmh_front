01bb2442e51633599dfa3d895c1ea305569ce581
tjrwodnjs <tjrwodnjs99@gmail.com>
2023-11-26 01:19:48 +0900

    fix: api

--- src/routes/ApiTest.jsx
+++ src/routes/ApiTest.jsx
@@ -2,23 +2,23 @@ import { useEffect } from "react";
 // import {signIn} from "../apis/api";
 import axios from "axios";
 import { getCookie } from "../utils/cookie";
+import { sendVerificationEmail } from "../apis/api";
 
 const ApiTest = () => {
   useEffect(() => {
-	  console.log("돼나?");
     const getPostAPI = async () => {
-	console.log("돼라");
-       const response = await axios.post("https://naemilham.site/api/accounts/send-verification-email/", 
-        {
-           "email": "idltkim@gmail.com",
-        }, 
-         {
-         headers: {
-           "Content-Type": "application/json",
-           "X-CSRFToken": getCookie("csrftoken"),
-         },
-         withCredentials: true
-       });
+      // const response = await axios.post("https://naemilham.site/api/accounts/send-verification-email/", 
+      //   {
+      //      "email": "idltkim@gmail.com",
+      //   }, 
+      //    {
+      //    headers: {
+      //      "Content-Type": "application/json",
+      //      "X-CSRFToken": getCookie("csrftoken"),
+      //    },
+      //    withCredentials: true
+      //  });
+      const response = await sendVerificationEmail({"email": "idltkim@gmail.com",});
       console.log(response);
     };
     getPostAPI();
